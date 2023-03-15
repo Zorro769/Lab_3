7c9f31b2858231a5478d126cb2b0fd9cda66f729
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2023-03-15 17:14:28 +0200

    adding classes

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Eagle obj_1 = new Eagle();
+        Duck obj_2 = new Duck();
+        obj_1.Eat();
+        obj_1.Move();
+        obj_2.Eat();
+        obj_2.Move();
+    }
+}
+abstract class Bird {
+    abstract void Eat();
+    abstract void Move();
+}
+
+class Eagle extends Bird{
+    @Override
+    void Eat() {
+        System.out.println("Eagle is eating ");
+    }
+
+    @Override
+    void Move() {
+        System.out.println("Eagle is moving ");
+    }
+}
+class Duck extends Bird{
+    @Override
+    void Move() {
+        System.out.println("Duck is moving ");
+    }
+
+    @Override
+    void Eat() {
+        System.out.println("Duck is eating");
     }
 }
