517355de0ff06e8cc0698b81a8989314c7b5b00d
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2023-03-16 17:16:42 +0200

    adding constructors

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,37 +1,81 @@
 public class Main {
     public static void main(String[] args) {
-        Eagle obj_1 = new Eagle();
-        Duck obj_2 = new Duck();
-        obj_1.Eat();
-        obj_1.Move();
-        obj_2.Eat();
-        obj_2.Move();
+        Eagle eagle_1 = new Eagle();
+        Eagle eagle_2 = new Eagle("duck");
+        Eagle eagle_3 = new Eagle("wolf",30.5);
+        Duck duck_1 = new Duck();
+        Duck duck_2 = new Duck("eagle");
+        Duck duck_3 = new Duck("worm",10.5);
+        System.out.println(eagle_1.Eat());
+        System.out.println(eagle_1.Move());
+        System.out.println(duck_1.Eat());
+        System.out.println(duck_1.Move());
+        System.out.println(eagle_2.Eat());
+        System.out.println(eagle_2.Move());
+        System.out.println(duck_2.Eat());
+        System.out.println(duck_2.Move());
+        System.out.println(eagle_3.Move());
+        System.out.println(eagle_3.Eat());
+        System.out.println(duck_3.Move());
+        System.out.println(duck_3.Eat());
     }
 }
 abstract class Bird {
-    abstract void Eat();
-    abstract void Move();
+    String eat;
+    abstract String Eat();
+    abstract String Move();
 }
 
 class Eagle extends Bird{
+
+    double flySpeed;
+    public Eagle() {
+        eat = "chickens";
+        flySpeed = 10;
+    }
+    public Eagle(String food,Double... speed)
+    {
+        eat = food;
+        if(speed.length > 0)
+            this.flySpeed = speed[0];
+        else
+            this.flySpeed = 10;
+    }
     @Override
-    void Eat() {
-        System.out.println("Eagle is eating ");
+    String Eat() {
+        return ("Eagle is eating " + eat);
     }
 
     @Override
-    void Move() {
-        System.out.println("Eagle is moving ");
+    String Move() {
+        return ("Eagle is moving with the speed: " + flySpeed);
     }
 }
 class Duck extends Bird{
+    double footSpeed;
+    Duck(){
+        eat = "Wheat";
+        footSpeed = 5;
+    }
+    Duck(String food,Double... speed)
+    {
+        eat = food;
+        if(speed.length > 0)
+        {
+            this.footSpeed = speed[0];
+        }
+        else
+        {
+            this.footSpeed = 10;
+        }
+    }
     @Override
-    void Move() {
-        System.out.println("Duck is moving ");
+    String Move() {
+        return ("Duck is moving with the speed " + footSpeed);
     }
 
     @Override
-    void Eat() {
-        System.out.println("Duck is eating");
+    String Eat() {
+        return ("Duck is eating " + eat);
     }
 }
